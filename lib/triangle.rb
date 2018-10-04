61973db890218a5833008700fa8f62ab616b6856
Travis T <travistokuyama@gmail.com>
2018-10-04 00:34:16 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,7 @@ class Triangle
   def kind 
     if side1 == 0 || side2 == 0 || side3 == 0 
       raise TriangleError
+    end
     if side1 == side2 && side2 == side3
       :equilateral
     elsif side1 == side2 || side2 == side3 || side1 == side3
